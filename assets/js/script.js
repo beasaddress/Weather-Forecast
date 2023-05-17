d78bdada75020fe675d4dd08f9a34ae2e2fe3340
Beatriz Sandoval <beasaddress@gmail.com>
2023-05-17 14:29:07 -0700

    pasted my api key from openWeatherAPI and created a function that will display the weather using the user input and a function calledcurrentWeather that i havent written yet...

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,24 @@ let currentWindSpeed = $("#windSpeed");
 //It will be useful holding this storage to do things like
 //clearing the search histpry, 
 let searchedCities = [];
-
+//creating a function that will search inside the storage for a city name
 function find(c){
     for (var i=0; i< searchedCities.length; i++) {
-        
+        if(c.toUpperCase()===searchedCities[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+//api key
+let APIKey = "d906d1b93882fa918015d052d274c520";
+//this function is going to display the five day forecast after getting the city name from the user
+//input and getting the data from the API
+function displayWeather(event){
+    event.preventDefault();//using preventDefault to stop the browser from displaying the wrong forecast from previous entries
+    if(searchedCity.val().trim()!==""){
+        city=searchedCity.val().trim();//using val() to retrive the "value" or what was inside the user input field.using trim() to get rid
+        //of "whitespace" like tabs, line breaks, or spaces or something from the user
+        currentWeather(city); //npw calling the function that grab the current weather from the api using the user input, we'll call it currentWeather
     }
 }
